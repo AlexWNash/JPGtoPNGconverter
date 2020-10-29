4e533081c962f638226e2c3f780b36fc3c490f78
Anonymously-Cognizant <nash.alexander.w@gmail.com>
2020-10-28 23:33:30 -0700

    Added an information box that tells the user how many images were successfully converted.

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -3,16 +3,19 @@ import sys
 from pathlib import Path
 from tkinter import Tk
 from tkinter.filedialog import askdirectory
-
+from tkinter.messagebox import showinfo
 from PIL import Image
 
 
 def jpg_to_png(jpg, png):
+    counter = 0
     for img in os.listdir(f'{jpg}'):
         if img.endswith('.jpg'):
             name = Path(f'{img}').stem
             img_obj = Image.open(f'{jpg}\\{img}')
             img_obj.save(f'{png}\\{name}.png', 'png')
+        counter += 1
+    showinfo(title='Conversion Complete', message=f'{counter} images were converted.')
 
 
 try:
